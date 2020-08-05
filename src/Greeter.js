745fb421f988fa81d28be581cb2584648421fa91
Chester Cabahug <chesterdeveloper2000@gmail.com>
2020-08-05 18:16:34 +0800

    event lister in react! you can do it in 2 ways, create a variable(better) or create the function directly

--- src/Greeter.js
+++ src/Greeter.js
@@ -1,12 +1,17 @@
 import React from "react"
 
 function Greeter({name, age=16, excitement = 1}) {
-return (   
-    <> 
-    <h1>Hi there, {name}{"!".repeat(excitement)}</h1>
-    <p>I know that you are {age} years old</p>
-    </>
-) 
+    const greet = () => {
+        alert(`Hello there, ${name}`)
+    }
+    return (   
+        <> 
+        <h1>Hi there, {name}{"!".repeat(excitement)}</h1>
+        <p>I know that you are {age} years old</p>
+        <button onClick={() => alert("hello!")}>Click Me!</button>
+        <button onClick={greet}>Click Me, I'm better</button>
+        </>
+    ) 
 }
 
 export default Greeter
