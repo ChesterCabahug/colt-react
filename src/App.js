dbeead7178f6a50573303030f815e14c8399c18b
Chester Cabahug <chesterdeveloper2000@gmail.com>
2020-08-05 17:36:34 +0800

    learned about props.. a fancy way of saying arguments in react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
+import Greeter from './Greeter';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Greeter name="Chester" />
       <h1>Hello from App!!!!</h1>
     </div>
   );
