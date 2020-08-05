c9195c2b19cb66278fa3667592a025737e0302f4
Chester Cabahug <chesterdeveloper2000@gmail.com>
2020-08-05 18:27:59 +0800

    intro to STATE.. it's what you use if you want to have 'a piece of info that changes'

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import Greeter from './Greeter';
+import Counter from "./Counter"
 
 function App() {
   return (
     <div>
+      <Counter/>
       <Navbar/>
       <Greeter name="Chester" age="19" excitement={4}/>
       <Greeter name="Chri" excitement={2}/>
