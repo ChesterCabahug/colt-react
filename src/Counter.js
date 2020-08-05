c33c77965424f37523832a157feaca1b3897e334
Chester Cabahug <chesterdeveloper2000@gmail.com>
2020-08-05 19:00:44 +0800

    styling method #2 (what is usually being used) putting className in a top level div / whatever's the parent that's the name of the component

--- src/Counter.js
+++ src/Counter.js
@@ -5,7 +5,7 @@ import "./Counter.css"
 const Counter = ({step = 1}) => {
     const [count, setCount] = useState(0)
     return (
-        <div>
+        <div className="Counter">
             <h2>{count}</h2>
             <button onClick={() => setCount(count + step)}>+{step}</button>
         </div>
